2a26d72601f63240520d8621dcb404578e709934
Phillip Strom <phillip.strom@gmail.com>
2020-05-07 17:43:38 -0600

    Create Turn and TurnTest classes

---
+++ test/turn_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require 'pry'
+
+class TurnTest < Minitest::Test
+end
