169ffa6ae2fffff609c2430dc7e6b1456599ebc8
Phillip Strom <phillip.strom@gmail.com>
2020-05-07 19:06:02 -0600

    Create all winner scenarios

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,18 +23,22 @@ attr_reader :player1,
     if type == :basic
       if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
         player1
-      elsif
+      else
         player2
       end
+    elsif type == :war
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+        player1
+      else
+        player2
+      end
+    else
+      "No Winner"
     end
   end
 
 end
 
 
-# create basic (method?). rank_of_card_at(0) is not equal
-# create war (method?). rank_of_card_at(0) are equal
-# create mutually_assured_destruction (method?). rank_of_card_at(0) and rank_of_card_at(2) are both equal
-# create winner method, results based on turn type
 # create pile_cards meethod. send cards from player's decks to @spoils_of_war
 # create award_spoils method. add cards from @spoils_of_war to the winner of the turn
