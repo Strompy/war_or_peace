687f6f80ef7ec52dce7fde4283e868160ccc5a9f
Phillip Strom <phillip.strom@gmail.com>
2020-05-09 16:32:14 -0600

    Add award_spoils method and basic test

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,20 +39,23 @@ attr_reader :player1,
 
   def pile_cards
     if type == :basic
+      @spoils_of_war << player1.deck.remove_card
       @spoils_of_war << player2.deck.remove_card
-      @spoils_of_war << player1.deck[0]
-      player1.deck.remove_card
     elsif type == :war
       3.times do
-        @spoils_of_war << player2.deck.remove_card
         @spoils_of_war << player1.deck.remove_card
+        @spoils_of_war << player2.deck.remove_card
       end
     else # :mutually_assured_destruction
       3.times do
-        player2.deck.remove_card
         player1.deck.remove_card
+        player2.deck.remove_card
       end
     end
+  end
+
+  def award_spoils(winner)
+    winner.deck.add_card(@spoils_of_war).flatten!
   end
 
 end
