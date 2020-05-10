cb1177cba9ff6299e3424437ea3ff9f34f022e77
Phillip Strom <phillip.strom@gmail.com>
2020-05-10 16:58:36 -0600

    Add looping game logic

--- lib/play.rb
+++ lib/play.rb
@@ -24,31 +24,39 @@ class Play
 
   def play
     turn = Turn.new(@player1, @player2)
-    winner = turn.winner
+    loop do
+      winner = turn.winner
 
-    if turn.type == :basic
+      if turn.type == :basic
+        turn.pile_cards
+        turn.award_spoils(winner)
+        p "Turn #{round}: #{winner.name} won 2 cards"
+      elsif turn.type == :war
+        turn.pile_cards
+        turn.award_spoils(winner)
+        p "Turn #{round}: #{winner.name} won 6 cards"
+      elsif turn.type == :mutually_assured_destruction
       turn.pile_cards
-      turn.award_spoils(winner)
-      p "Turn #{round}: #{winner.name} won 2 cards"
+        p "Turn #{round}: *mutually assured destruction* 6 cards removed from play"
+      end
+      @round += 1
+      break if @round == 1000000 || player1.has_lost? || player2.has_lost?
     end
-    if turn.type == :war
-      turn.pile_cards
-      turn.award_spoils(winner)
-      p "Turn #{round}: #{winner.name} won 6 cards"
-    end
-    if turn.type == :mutually_assured_destruction
-      turn.pile_cards
-      p "Turn #{round}: *mutually assured destruction* 6 cards removed from play"
-    end
-    @round += 1
+    endgame
   end
 
+  def endgame
+    if @round == 1000000
+      p "All right. We'll call it a draw"
+    elsif player1.has_lost?
+      p "*~*~*~* #{player2.name} has won the game! *~*~*~*"
+    else
+      p "*~*~*~* #{player1.name} has won the game! *~*~*~*"
+    end
+  end
 end
 
-# create gameplay loop
 
-# puts on start:
-# Welcome to War! (or Peace) This game will be played with 52 cards.
-# The players today are Megan and Aurora.
-# Type 'GO' to start the game!
-# ------------------------------------------------------------------
+
+
+# create gameplay loop
