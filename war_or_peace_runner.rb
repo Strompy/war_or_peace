e6ea3feeb8c672cda41c4edd1b08d1cbb37679f5
Phillip Strom <phillip.strom@gmail.com>
2020-05-10 21:08:40 -0600

    Make program rungit add war_or_peace_runner.rb

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,5 +1,3 @@
-require 'minitest/autorun'
-require 'minitest/pride'
 require './lib/card'
 require './lib/deck'
 require './lib/player'
@@ -7,12 +5,73 @@ require './lib/turn'
 require './lib/play'
 require 'pry'
 
+cards = []
+cards << card1 = Card.new(:heart, 'Ace', 14)
+cards << card2 = Card.new(:heart, 'King', 13)
+cards << card3 = Card.new(:heart, 'Queen', 12)
+cards << card4 = Card.new(:heart, 'Jack', 11)
+cards << card5 = Card.new(:heart, '10', 10)
+cards << card6 = Card.new(:heart, '9', 9)
+cards << card7 = Card.new(:heart, '8', 8)
+cards << card8 = Card.new(:heart, '7', 7)
+cards << card9 = Card.new(:heart, '6', 6)
+cards << card10 = Card.new(:heart, '5', 5)
+cards << card11 = Card.new(:heart, '4', 4)
+cards << card12 = Card.new(:heart, '3', 3)
+cards << card13 = Card.new(:heart, '2', 2)
+cards << card14 = Card.new(:diamond, 'Ace', 14)
+cards << card15 = Card.new(:diamond, 'King', 13)
+cards << card16 = Card.new(:diamond, 'Queen', 12)
+cards << card17 = Card.new(:diamond, 'Jack', 11)
+cards << card18 = Card.new(:diamond, '10', 10)
+cards << card19 = Card.new(:diamond, '9', 9)
+cards << card20 = Card.new(:diamond, '8', 8)
+cards << card21 = Card.new(:diamond, '7', 7)
+cards << card22 = Card.new(:diamond, '6', 6)
+cards << card23 = Card.new(:diamond, '5', 5)
+cards << card24 = Card.new(:diamond, '4', 4)
+cards << card25 = Card.new(:diamond, '3', 3)
+cards << card26 = Card.new(:diamond, '2', 2)
+cards << card27 = Card.new(:club, 'Ace', 14)
+cards << card28 = Card.new(:club, 'King', 13)
+cards << card29 = Card.new(:club, 'Queen', 12)
+cards << card30 = Card.new(:club, 'Jack', 11)
+cards << card31 = Card.new(:club, '10', 10)
+cards << card32 = Card.new(:club, '9', 9)
+cards << card33 = Card.new(:club, '8', 8)
+cards << card34 = Card.new(:club, '7', 7)
+cards << card35 = Card.new(:club, '6', 6)
+cards << card36 = Card.new(:club, '5', 5)
+cards << card37 = Card.new(:club, '4', 4)
+cards << card38 = Card.new(:club, '3', 3)
+cards << card39 = Card.new(:club, '2', 2)
+cards << card40 = Card.new(:spade, 'Ace', 14)
+cards << card41 = Card.new(:spade, 'King', 13)
+cards << card42 = Card.new(:spade, 'Queen', 12)
+cards << card43 = Card.new(:spade, 'Jack', 11)
+cards << card44 = Card.new(:spade, '10', 10)
+cards << card45 = Card.new(:spade, '9', 9)
+cards << card46 = Card.new(:spade, '8', 8)
+cards << card47 = Card.new(:spade, '7', 7)
+cards << card48 = Card.new(:spade, '6', 6)
+cards << card49 = Card.new(:spade, '5', 5)
+cards << card50 = Card.new(:spade, '4', 4)
+cards << card51 = Card.new(:spade, '3', 3)
+cards << card52 = Card.new(:spade, '2', 2)
 
+cards.shuffle!
 
+deck = Deck.new(cards)
 
+split_deck1 = deck.cards.slice(0..25)
+split_deck2 = deck.cards.slice(26..51)
 
-# Create deck
-# Shuffle deck
-# Split deck into two
-# Create two players and give each a half-deck
-# run start method
+deck1 = Deck.new(split_deck1)
+deck2 = Deck.new(split_deck2)
+
+player1 = Player.new("Megan", deck1)
+player2 = Player.new("Aurora", deck2)
+
+play = Play.new(player1, player2)
+
+play.start
