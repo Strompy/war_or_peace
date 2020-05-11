76ab3cca5f0581db2fe068db64d3995a3c85bebf
Phillip Strom <phillip.strom@gmail.com>
2020-05-10 19:50:11 -0600

    Update requires

---
+++ war_or_peace_runner.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require './lib/play'
+require 'pry'
+
+
+
+
+
+# Create deck
+# Shuffle deck
+# Split deck into two
+# Create two players and give each a half-deck
+# run start method
